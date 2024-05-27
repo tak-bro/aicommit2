8ebb2fe29f6cf871a5f89f9995451406754db468
tak <env.tak@gmail.com>
2024-05-27 14:50:14 +0900

    refactor: add generalConfigParsers

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -23,6 +23,127 @@ const parseAssert = (name: string, condition: any, message: string) => {
     }
 };
 
+const generalConfigParsers = {
+    confirm(confirm?: string | boolean) {
+        if (!confirm) {
+            return false;
+        }
+
+        if (typeof confirm === 'boolean') {
+            return confirm;
+        }
+
+        parseAssert('confirm', /^(?:true|false)$/.test(confirm), 'Must be a boolean');
+        return confirm === 'true';
+    },
+    prompt(prompt?: string) {
+        if (!prompt) {
+            return '';
+        }
+        return prompt;
+    },
+    locale(locale?: string) {
+        if (!locale) {
+            return 'en';
+        }
+
+        parseAssert('locale', locale, 'Cannot be empty');
+        parseAssert(
+            'locale',
+            /^[a-z-]+$/i.test(locale),
+            'Must be a valid locale (letters and dashes/underscores). You can consult the list of codes in: https://wikipedia.org/wiki/List_of_ISO_639-1_codes'
+        );
+        return locale;
+    },
+    generate(count?: string) {
+        if (!count) {
+            return 1;
+        }
+
+        parseAssert('generate', /^\d+$/.test(count), 'Must be an integer');
+
+        const parsed = Number(count);
+        parseAssert('generate', parsed > 0, 'Must be greater than 0');
+        parseAssert('generate', parsed <= 5, 'Must be less or equal to 5');
+
+        return parsed;
+    },
+    type(type?: CommitType) {
+        if (!type) {
+            return 'conventional';
+        }
+
+        parseAssert('type', commitTypes.includes(type as CommitType), 'Invalid commit type');
+
+        return type as CommitType;
+    },
+    proxy(url?: string) {
+        if (!url || url.length === 0) {
+            return undefined;
+        }
+
+        parseAssert('proxy', /^https?:\/\//.test(url), 'Must be a valid URL');
+
+        return url;
+    },
+    timeout(timeout?: string) {
+        if (!timeout) {
+            return 10_000;
+        }
+
+        parseAssert('timeout', /^\d+$/.test(timeout), 'Must be an integer');
+
+        const parsed = Number(timeout);
+        parseAssert('timeout', parsed >= 500, 'Must be greater than 500ms');
+
+        return parsed;
+    },
+    temperature(temperature?: string) {
+        if (!temperature) {
+            return 0.7;
+        }
+
+        parseAssert('temperature', /^(2|\d)(\.\d{1,2})?$/.test(temperature), 'Must be decimal between 0 and 2');
+
+        const parsed = Number(temperature);
+        parseAssert('temperature', parsed > 0.0, 'Must be greater than 0');
+        parseAssert('temperature', parsed <= 2.0, 'Must be less than or equal to 2');
+
+        return parsed;
+    },
+    'max-length'(maxLength?: string) {
+        if (!maxLength) {
+            return 50;
+        }
+
+        parseAssert('max-length', /^\d+$/.test(maxLength), 'Must be an integer');
+
+        const parsed = Number(maxLength);
+        parseAssert('max-length', parsed >= 20, 'Must be greater than 20 characters');
+
+        return parsed;
+    },
+    'max-tokens'(maxTokens?: string) {
+        if (!maxTokens) {
+            return 200;
+        }
+
+        parseAssert('max-tokens', /^\d+$/.test(maxTokens), 'Must be an integer');
+        return Number(maxTokens);
+    },
+    logging(enable?: string | boolean) {
+        if (!enable) {
+            return false;
+        }
+        if (typeof enable === 'boolean') {
+            return enable;
+        }
+
+        parseAssert('logging', /^(?:true|false)$/.test(enable), 'Must be a boolean(true or false)');
+        return enable === 'true';
+    },
+} as const;
+
 const configParsers = {
     OPENAI_KEY(key?: string) {
         if (!key) {
@@ -183,135 +304,16 @@ const configParsers = {
         parseAssert('OLLAMA_STREAM', /^(?:true|false)$/.test(stream), 'Must be a boolean(true or false)');
         return stream === 'true';
     },
-    confirm(confirm?: string | boolean) {
-        if (!confirm) {
-            return false;
-        }
-
-        if (typeof confirm === 'boolean') {
-            return confirm;
-        }
-
-        parseAssert('confirm', /^(?:true|false)$/.test(confirm), 'Must be a boolean');
-        return confirm === 'true';
-    },
-    prompt(prompt?: string) {
-        if (!prompt) {
-            return '';
-        }
-        return prompt;
-    },
-    locale(locale?: string) {
-        if (!locale) {
-            return 'en';
-        }
-
-        parseAssert('locale', locale, 'Cannot be empty');
-        parseAssert(
-            'locale',
-            /^[a-z-]+$/i.test(locale),
-            'Must be a valid locale (letters and dashes/underscores). You can consult the list of codes in: https://wikipedia.org/wiki/List_of_ISO_639-1_codes'
-        );
-        return locale;
-    },
-    generate(count?: string) {
-        if (!count) {
-            return 1;
-        }
-
-        parseAssert('generate', /^\d+$/.test(count), 'Must be an integer');
-
-        const parsed = Number(count);
-        parseAssert('generate', parsed > 0, 'Must be greater than 0');
-        parseAssert('generate', parsed <= 5, 'Must be less or equal to 5');
-
-        return parsed;
-    },
-    type(type?: CommitType) {
-        if (!type) {
-            return 'conventional';
-        }
-
-        parseAssert('type', commitTypes.includes(type as CommitType), 'Invalid commit type');
-
-        return type as CommitType;
-    },
-    proxy(url?: string) {
-        if (!url || url.length === 0) {
-            return undefined;
-        }
-
-        parseAssert('proxy', /^https?:\/\//.test(url), 'Must be a valid URL');
-
-        return url;
-    },
-    timeout(timeout?: string) {
-        if (!timeout) {
-            return 10_000;
-        }
-
-        parseAssert('timeout', /^\d+$/.test(timeout), 'Must be an integer');
-
-        const parsed = Number(timeout);
-        parseAssert('timeout', parsed >= 500, 'Must be greater than 500ms');
-
-        return parsed;
-    },
-    temperature(temperature?: string) {
-        if (!temperature) {
-            return 0.7;
-        }
-
-        parseAssert('temperature', /^(2|\d)(\.\d{1,2})?$/.test(temperature), 'Must be decimal between 0 and 2');
-
-        const parsed = Number(temperature);
-        parseAssert('temperature', parsed > 0.0, 'Must be greater than 0');
-        parseAssert('temperature', parsed <= 2.0, 'Must be less than or equal to 2');
-
-        return parsed;
-    },
-    'max-length'(maxLength?: string) {
-        if (!maxLength) {
-            return 50;
-        }
-
-        parseAssert('max-length', /^\d+$/.test(maxLength), 'Must be an integer');
-
-        const parsed = Number(maxLength);
-        parseAssert('max-length', parsed >= 20, 'Must be greater than 20 characters');
-
-        return parsed;
-    },
-    'max-tokens'(maxTokens?: string) {
-        if (!maxTokens) {
-            return 200;
-        }
-
-        parseAssert('max-tokens', /^\d+$/.test(maxTokens), 'Must be an integer');
-        const parsed = Number(maxTokens);
-        return parsed;
-    },
-    logging(enable?: string | boolean) {
-        if (!enable) {
-            return false;
-        }
-        if (typeof enable === 'boolean') {
-            return enable;
-        }
-
-        parseAssert('logging', /^(?:true|false)$/.test(enable), 'Must be a boolean(true or false)');
-        return enable === 'true';
-    },
 } as const;
 
-type ConfigKeys = keyof typeof configParsers;
+type ConfigKeys = keyof typeof generalConfigParsers | keyof typeof configParsers;
 
 type RawConfig = {
     [key in ConfigKeys]?: string;
 };
 
 export type ValidConfig = {
-    [Key in ConfigKeys]: ReturnType<(typeof configParsers)[Key]>;
+    [Key in ConfigKeys]: ReturnType<(typeof generalConfigParsers & typeof configParsers)[Key]>;
 };
 
 const configPath = path.join(os.homedir(), '.aicommit2');
@@ -330,8 +332,10 @@ export const getConfig = async (cliConfig?: RawConfig, suppressErrors?: boolean)
     const config = await readConfigFile();
     const parsedConfig: Record<string, unknown> = {};
 
-    for (const key of Object.keys(configParsers) as ConfigKeys[]) {
-        const parser = configParsers[key];
+    const allConfigParsers = { ...generalConfigParsers, ...configParsers };
+
+    for (const key of Object.keys(allConfigParsers) as ConfigKeys[]) {
+        const parser = allConfigParsers[key];
         const value: any = cliConfig?.[key] ?? config[key];
 
         if (suppressErrors) {
@@ -350,13 +354,14 @@ export const getConfig = async (cliConfig?: RawConfig, suppressErrors?: boolean)
 
 export const setConfigs = async (keyValues: [key: string, value: any][]) => {
     const config = await readConfigFile();
+    const allConfigParsers = { ...generalConfigParsers, ...configParsers };
 
     for (const [key, value] of keyValues) {
-        if (!hasOwn(configParsers, key)) {
+        if (!hasOwn(config, key)) {
             throw new KnownError(`Invalid config property: ${key}`);
         }
 
-        const parsed = configParsers[key as ConfigKeys](value);
+        const parsed = allConfigParsers[key as ConfigKeys](value);
         config[key as ConfigKeys] = parsed as any;
     }
 
