e5d407b8ab6f520571faee5d56d950177c8f623d
tak <env.tak@gmail.com>
2024-08-22 12:49:40 +0900

    chore: exclude files ending with '.lockb' from diff

--- src/utils/git.ts
+++ src/utils/git.ts
@@ -24,6 +24,7 @@ const filesToExclude = [
     'pnpm-lock.yaml',
     // yarn.lock, Cargo.lock, Gemfile.lock, Pipfile.lock, etc.
     '*.lock',
+    '*.lockb',
     '*.gif',
     '*.png',
 ].map(excludeFromDiff);
