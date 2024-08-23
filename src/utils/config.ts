49663d736a969d47f304455234623cc9d570f21e
ryicoh <ryicoh@gmail.com>
2024-08-23 10:53:28 +0900

    update groq model

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -349,7 +349,20 @@ const configParsers = {
         if (!model || model.length === 0) {
             return 'gemma-7b-it';
         }
-        const supportModels = [`llama3-8b-8192`, 'llama3-70b-8192', `mixtral-8x7b-32768`, `gemma-7b-it`];
+
+        const supportModels = [
+            'gemma2-9b-it',
+            'gemma-7b-it',
+            'llama-3.1-70b-versatile',
+            'llama-3.1-8b-instant',
+            'llama3-70b-8192',
+            'llama3-8b-8192',
+            'llama3-groq-70b-8192-tool-use-preview',
+            'llama3-groq-8b-8192-tool-use-preview',
+            'llama-guard-3-8b',
+            'mixtral-8x7b-32768',
+        ];
+
         parseAssert('GROQ_MODEL', supportModels.includes(model), 'Invalid model type of Groq');
         return model;
     },
