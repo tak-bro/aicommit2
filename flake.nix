55ec9d63312452db46d09e9476098cedb0a5020b
tak-bro <7614353+tak-bro@users.noreply.github.com>
2025-10-24 04:42:33 +0000

    chore(nix): update flake.nix to version v2.4.12 [skip ci]

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
       perSystem = {pkgs, ...}: {
         packages.default = pkgs.stdenvNoCC.mkDerivation (finalAttrs: {
           pname = "aicommit2";
-          version = "v2.4.11";
+          version = "v2.4.12";
           src = self;
 
           pnpmDeps = pkgs.pnpm.fetchDeps {
